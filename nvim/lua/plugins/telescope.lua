cd1295d7b265dad00d08a4d98375900992091f3d
kostas rekoumis <konstantinos.rekoumis@gmail.com>
2025-02-25 14:56:13 +0200

    Revised Telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,9 +16,11 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fo", builtin.find_files, {})
+      vim.keymap.set("n", "<C-P>", builtin.builtin, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 
       require("telescope").load_extension("ui-select")
     end,
